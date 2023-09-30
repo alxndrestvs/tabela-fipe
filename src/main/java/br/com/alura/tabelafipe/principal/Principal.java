d04bedce116fd122d6b0556bcb12bea6789b1578
Alexandre Esteves <alexandreestevesfclaro@gmail.com>
2023-09-29 23:48:05 -0300

    Melhora da funcionalidade de acordo com a aula "Buscando todas as avaliações por veículo"

--- src/main/java/br/com/alura/tabelafipe/principal/Principal.java
+++ src/main/java/br/com/alura/tabelafipe/principal/Principal.java
@@ -4,9 +4,11 @@ import br.com.alura.tabelafipe.model.*;
 import br.com.alura.tabelafipe.service.ConsumoAPI;
 import br.com.alura.tabelafipe.service.ConverteDados;
 
+import java.util.ArrayList;
 import java.util.Comparator;
 import java.util.List;
 import java.util.Scanner;
+import java.util.stream.Collectors;
 
 public class Principal {
 
@@ -34,17 +36,30 @@ public class Principal {
                 .sorted(Comparator.comparing(Dados::codigo))
                 .forEach(System.out::println);
 
-        System.out.println("Informe o código do modelo escolhido: ");
+        System.out.println("Informe um trecho do nome do carro a ser buscado: ");
         var modeloEscolhido = sc.nextLine();
-        json = consumo.obterDados("https://parallelum.com.br/fipe/api/v1/" + veiculoEscolhido + "/marcas/" + marcaEscolhida + "/modelos/" + modeloEscolhido + "/anos");
-        var dadosAnos = conversor.obterLista(json, Dados.class);
-        dadosAnos.forEach(System.out::println);
 
-        System.out.println("Informe o ano escolhido: ");
-        var anoEscolhido = sc.nextLine();
-        json = consumo.obterDados("https://parallelum.com.br/fipe/api/v1/" + veiculoEscolhido + "/marcas/" + marcaEscolhida + "/modelos/" + modeloEscolhido + "/anos/" + anoEscolhido);
-        DadosVeiculo dadosVeiculo = conversor.obterDados(json, DadosVeiculo.class);
-        System.out.println(dadosVeiculo);
+        List<Dados> modelosFiltrados = dadosModelos.modelos().stream()
+                .filter(m -> m.nome().toLowerCase().contains(modeloEscolhido.toLowerCase()))
+                .collect(Collectors.toList());
+
+        System.out.println("Modelos filtrados");
+        modelosFiltrados.forEach(System.out::println);
+
+        System.out.println("Informe o código do modelo escolhido: ");
+        var codigoModelo = sc.nextLine();
+        json = consumo.obterDados("https://parallelum.com.br/fipe/api/v1/" + veiculoEscolhido + "/marcas/" + marcaEscolhida + "/modelos/" + codigoModelo + "/anos");
+        var dadosAnos = conversor.obterLista(json, Dados.class);
+        List<DadosVeiculo> veiculos = new ArrayList<>();
+
+        for (int i = 0; i < dadosAnos.size(); i++) {
+            json = consumo.obterDados("https://parallelum.com.br/fipe/api/v1/" + veiculoEscolhido + "/marcas/" + marcaEscolhida + "/modelos/" + codigoModelo + "/anos/" + dadosAnos.get(i).codigo());
+            DadosVeiculo veiculo = conversor.obterDados(json, DadosVeiculo.class);
+            veiculos.add(veiculo);
+        }
+
+        System.out.println("Todos os veículos filtrados com avaliações por ano: ");
+        veiculos.forEach(System.out::println);
 
     }
 }
